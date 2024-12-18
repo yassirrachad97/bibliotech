c621c1aff59ce8ababb83de277c3ba0467d2109d
Yassir Rachad <yassirrachad1997@gmail.com>
2024-12-18 21:41:59 +0100

    implimentation cognito avec mon application et ajouter navbar pour la button connexion

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from "react-oidc-context";
+
+
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_3dLSBkiYv",
+  client_id: "1487qlgi773gpg7gq7sbn3gsa5",
+  redirect_uri: "http://localhost:5173/home",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
     <App />
+    </AuthProvider>
   </StrictMode>,
 )
