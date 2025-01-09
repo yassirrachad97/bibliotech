c1004a1e3fee641881ef3a12cf89158804711941
Yassir Rachad <yassirrachad1997@gmail.com>
2025-01-09 15:32:31 +0100

    faire hoste du bakend

--- backend/src/emprunt/emprunt.module.ts
+++ backend/src/emprunt/emprunt.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmpruntService } from './emprunt.service';
 import { EmpruntController } from './emprunt.controller';
+import { DynamoDBModule } from 'src/dynamodb/dynamodb.module';
 
 @Module({
+  imports: [DynamoDBModule],
   providers: [EmpruntService],
   controllers: [EmpruntController]
 })
--- backend/src/emprunt/emprunt.service.ts
+++ backend/src/emprunt/emprunt.service.ts
@@ -1,38 +1,44 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { ConfigService } from '@nestjs/config';
-import {v4 as uuidv4} from 'uuid';
-import {CreateEmpruntDto} from "./DTO/create-emprunt.dto";
+import { v4 as uuidv4 } from 'uuid';
+import { CreateEmpruntDto } from './DTO/create-emprunt.dto';
 import { QueryCommand } from '@aws-sdk/client-dynamodb';
+import { AttributeValue } from '@aws-sdk/client-dynamodb';
 
 @Injectable()
 export class EmpruntService {
-    constructor(
-        private readonly dynamoDBDocumentClient: DynamoDBDocumentClient,
-        private readonly configService: ConfigService,
-    ){}
-    async createEmprunt(createEmpruntDto: CreateEmpruntDto) {
-        const { userId, bookId, startDate, endDate, returned } = createEmpruntDto;
-        const tableName = this.configService.get<string>('EMPRUNT_TABLE_NAME');
+  constructor(
+    @Inject('DYNAMODB_CLIENT')
+    private readonly dynamoDbClient: DynamoDBDocumentClient,
+    private readonly configService: ConfigService,
+  ) {}
 
-        const bookAvaliable = await this.checkBookAvailability(bookId);
-        if (!bookAvaliable) {
-            throw new BadRequestException('Book is not available');
-        }
-        const emprunt = {
-            id: uuidv4(),
-            userId,
-            bookId,
-            startDate,
-            endDate,
-            returned,
-          };
+  async createEmprunt(createEmpruntDto: CreateEmpruntDto) {
+    try {
+      const { userId, bookId, startDate, endDate, returned } = createEmpruntDto;
+      const tableName = this.configService.get<string>('EMPRUNT_TABLE_NAME');
 
-          const params = {
-            TableName: tableName,
-            Item: emprunt,
-          };
-        await this.dynamoDBDocumentClient.send(new PutCommand(params));
+      const bookAvailable = await this.checkBookAvailability(bookId);
+      if (!bookAvailable) {
+        throw new BadRequestException('Book is not available');
+      }
+
+      const emprunt = {
+        id: uuidv4(),
+        userId,
+        bookId,
+        startDate,
+        endDate,
+        returned,
+      };
+
+      const params = {
+        TableName: tableName,
+        Item: emprunt,
+      };
+
+      await this.dynamoDbClient.send(new PutCommand(params));
       return {
         success: true,
         message: 'Emprunt enregistré avec succès',
@@ -40,34 +46,35 @@ export class EmpruntService {
       };
     } catch (error) {
       console.error('Erreur DynamoDB:', error);
-      throw new Error('Échec de la création de l’emprunt');
+      throw new Error("Échec de la création de l'emprunt");
     }
-
-    async checkBookAvailability(bookId: string): Promise<boolean> {
-        const tableName = this.configService.get<string>('EMPRUNT_TABLE_NAME');
-    
-        const params = {
-          TableName: tableName,
-          IndexName: 'bookId-index', 
-          KeyConditionExpression: 'bookId = :bookId AND #returned = :returned',
-          ExpressionAttributeValues: {
-            ':bookId': bookId, 
-            ':returned': false, 
-          },
-          ExpressionAttributeNames: {
-            '#returned': 'returned', 
-          },
-        };
-    
-        try {
-          const { Items } = await this.dynamoDBDocumentClient.send(new QueryCommand(params));
-          return Items.length === 0; 
-        } catch (error) {
-          console.error('Erreur DynamoDB:', error);
-          throw new Error('Échec de la vérification de disponibilité du livre');
-        }
-    }
-    
   }
-    
 
+  async checkBookAvailability(bookId: string): Promise<boolean> {
+    const tableName = this.configService.get<string>('EMPRUNT_TABLE_NAME');
+
+    const params = {
+      TableName: tableName,
+      IndexName: 'bookId-index',
+      KeyConditionExpression: 'bookId = :bookId',
+      FilterExpression: '#returned = :returned',
+      ExpressionAttributeValues: {
+        ':bookId': { S: bookId } as AttributeValue,
+        ':returned': { BOOL: false } as AttributeValue,
+      },
+      ExpressionAttributeNames: {
+        '#returned': 'returned',
+      },
+    };
+
+    try {
+      const { Items } = await this.dynamoDbClient.send(
+        new QueryCommand(params),
+      );
+      return Items?.length === 0;
+    } catch (error) {
+      console.error('Erreur DynamoDB:', error);
+      throw new Error('Échec de la vérification de disponibilité du livre');
+    }
+  }
+}
